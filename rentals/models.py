1082caefbfc8fe7eda6bfb4c3c25faff1bc97b97
Mprokolis <nikfragia06@gmail.com>
2025-08-17 16:55:25 +0300

    Add migrations for booking code

--- rentals/models.py
+++ rentals/models.py
@@ -75,6 +75,12 @@ class Booking(models.Model):
     customer_name = models.CharField(max_length=120, blank=True)
     customer_email = models.EmailField(blank=True)
     customer_phone = models.CharField(max_length=50, blank=True)
+    booking_code = models.CharField(
+        max_length=20,
+        blank=True,
+        unique=True,
+        db_index=True,
+    )
 
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
@@ -94,6 +100,13 @@ class Booking(models.Model):
 
     def __str__(self):
         return f"Booking #{self.id} ({self.status})"
+
+    def save(self, *args, **kwargs):
+        creating = self.pk is None
+        super().save(*args, **kwargs)
+        if creating and not self.booking_code:
+            self.booking_code = f"G5{self.id}"
+            super().save(update_fields=["booking_code"])
 
     @property
     def days(self) -> int:
